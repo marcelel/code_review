042808cbfb35450ad8fe0a36fb77de9c739eb80f
marcelel <marcelel321@gmail.com>
2016-07-16 13:08:00 +0200

    array

---
+++ array.h
@@ -0,0 +1,82 @@
+#pragma once
+#include <iostream>
+
+template<class T>
+class array_T
+{
+	T* arr;
+	int counter;
+public:	
+	array_T() :
+		counter(0),
+		arr(nullptr)
+	{}
+	~array_T()
+	{
+		if (counter != 0)
+			delete[] arr;
+	}
+	void add(T &);
+	bool del(int);
+	array_T<T>& operator=(const array_T<T> & tmp)
+	{
+		if (tmp.counter != 0)
+		{
+			if (arr != nullptr)
+				delete[] arr;
+			counter = tmp.counter;
+			arr = new T[counter];
+			for (int i = 0; i < counter; i++)
+				arr[i] = tmp.arr[i];
+		}
+		return *this;
+	}
+	friend std::ostream & operator<<(std::ostream & output, const array_T<T> & tmp)
+	{
+		for (int i = 0; i < tmp.counter; i++)
+			output << tmp.arr[i] << std::endl;
+		return output;
+	}
+};
+
+
+template <class T>
+void array_T<T>::add(T & added)
+{
+	if (counter == 0)
+	{
+		arr = new T[++counter];
+		arr[0] = added;
+	}
+	else
+	{
+		T* tmp = new T[++counter];
+		for (int i = 0; i < counter - 1; i++)
+			tmp[i] = arr[i];
+		tmp[counter - 1] = added;
+		delete[] arr;
+		arr = tmp;
+	}
+}
+
+template <class T>
+bool array_T<T>::del(int deleted)
+{
+	if (deleted < 0 || deleted >= counter)
+		return false;
+	else
+	{
+		T* tmp = new T[--counter];
+		for (int i = 0; i < deleted; i++)
+			tmp[i] = arr[i];
+		for (int i = deleted; i < counter; i++)
+			tmp[i] = arr[i + 1];
+		delete[] arr;
+		arr = tmp;
+	}
+}
+
+
+
+
+
